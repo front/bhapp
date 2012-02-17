1a2f23310b39ba14e2a3f5bb6a02a46428e8e391
Henrik Akselsen <henrik.akselsen@gmail.com>
2012-02-17 11:27:31 +0100

    Fixed Driving distance

--- js/functions.js
+++ js/functions.js
@@ -1,80 +1,113 @@
+var THORANDRE_LAT = 59.22370640;
+var THORANDRE_LON = 10.9481590;
+
 function SetCurrentLocation(){ navigator.geolocation.getCurrentPosition(SetCurrentLocation_OnSuccess, SetCurrentLocation_OnError,{maximumAge:0, timeout:15000}); }
 
 function SetCurrentLocation_OnSuccess(position) {
-	sLatitude = position.coords.latitude;
-	sLongitude = position.coords.longitude;
-	//alert("Din Posisjon er " + sLatitude + " + "+ sLongitude );
+	//sLatitude = position.coords.latitude;
+	//sLongitude = position.coords.longitude;
 	
-	var mapcanvas = document.createElement('div');
-	mapcanvas.id = 'mapcanvas';
-	mapcanvas.style.height = '400px'; // Cannot set to % ?!
-	mapcanvas.style.width = '100%';
-
-	document.querySelector('div#loc div.ui-content').appendChild(mapcanvas);
-
-	//alert( "useragent:" + useragent.indexOf('iPhone') );
-  var latlng = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
+	
+	//Build the DOM
+	$('div#loc div.ui-content').append("<div id='mapcanvas' style='height:400px' style='width:100%'></div");
+	$('div#loc div.ui-content').append("<ul id='mapinfo' data-role='listview' data-inset='true'></ul").trigger("create");
+	
+	// Build posistions
+	var myPosition = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
+	var ThorAndrePosition = new google.maps.LatLng(THORANDRE_LAT,THORANDRE_LON);
+	
+	// Build the Map
   var myOptions = {
 	    zoom: 16,
-	    center: latlng,
+	    center: myPosition,
 	    mapTypeControl: false,
 	    navigationControlOptions: {style: google.maps.NavigationControlStyle.SMALL},
 	    mapTypeId: google.maps.MapTypeId.ROADMAP
 	  };
-	  var map = new google.maps.Map(document.getElementById("mapcanvas"), myOptions);
+	var map = new google.maps.Map(document.getElementById("mapcanvas"), myOptions);
 
-	  var marker = new google.maps.Marker({
-	      position: latlng, 
-	      map: map, 
-	      title:"Du er her! (Med " + position.coords.accuracy + " meters nøyaktighet)"
+  // Build Markers
+	var marker = new google.maps.Marker({
+	  position: myPosition, 
+	  map: map, 
+	  title:"Du er her! (Med " + position.coords.accuracy + " meters nøyaktighet)"
 	  });
-	var distance  = CalculateDistance(position.coords.latitude, position.coords.longitude, 59.132534, 10.565337)/10;
-	alert("Du er " + distance + " km fra huset til Thor Andre (i luftlinje). Stikk innom da vel!");
+	  
+  var marker = new google.maps.Marker({
+	  position: ThorAndrePosition, 
+	  map: map, 
+	  title:"Thor Andre"
+	  });
+	  
+	// Build the Air distance and add the result to the DOM
+	var air_distance  = CalculateAirDistance(position.coords.latitude, position.coords.longitude, THORANDRE_LAT, THORANDRE_LON);
+	$('ul#mapinfo').append("<li>Du er " + air_distance + " km fra huset til Thor Andre i luftlinje</li>");
+	$('ul#mapinfo').listview("refresh");
   
+  // Call asynchmethod SetDriveDistance to calculate driving distance and then add it to the DOM
+	SetTravelDistance(position.coords.latitude, position.coords.longitude, THORANDRE_LAT, THORANDRE_LON);		
 }
 
 function SetCurrentLocation_OnError(error) {
 		alert("Location not found");
 }
+
 function getImages( GALLERY_URI, SERVICE_URI){
 
   $.getJSON(SERVICE_URI, function(data){
 
   // For each node, add some html to the DOM
 	  $.each(data.nodes, function(){
-		  var img_full =  this.node.full;
-		  var img_thumb = this.node.thumb;
-		  var img_title = this.node.title;
-		
-		  var new_img = "<li><a href='" + img_full + "' rel='external'><img src='" + img_thumb + "' alt='" + img_title + "' /></a></li>";
+		  var new_img = "<li><a href='" + this.node.full + "' rel='external'><img src='" + 
+		                this.node.thumb + "' alt='" + this.node.title + "' /></a></li>";
+		  
 		  $("#Gallery1 .gallery").append( new_img);					
-	    }); // end each
-
-    }); //end getJSON()
+    }); 
+  }); 
 }
 
-function toRad(deg) {
-    return deg * Math.PI / 180;
-  }
-
-/*
-function CalculateDistance(lat1,lon1,lat2,lon2){
-  var R = 6371; // Radius of the earth in km
-  var dLat = (lat2-lat1).toRad();  // Javascript functions in radians
-  var dLon = (lon2-lon1).toRad(); 
-  var a = Math.sin(dLat/2) * Math.sin(dLat/2) + Math.cos(lat1.toRad()) * Math.cos(lat2.toRad()) * Math.sin(dLon/2) * Math.sin(dLon/2); 
-  var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a)); 
-  var d = R * c; // Distance in km
-  alert("here");
+function SetTravelDistance(lat1,lon1,lat2,lon2){
   
-  return d;
-} */
+  var url = "http://maps.googleapis.com/maps/api/directions/json";
+  var data =  "origin=" + lat1 + "," + lon1 + 
+              "&destination=" + lat2 + "," + lon2 + 
+              "&sensor=false";
+  
+	var request = $.ajax({
+		type:"GET",
+		url: url,
+		data: data,
+		dataType: "json"
+		});
+	
+	// Handle the data from Google
+	request.success( function( response ){
+	  
+	  // Get the distance from the object
+	  distance =  response.routes[0].legs[0].distance.value;
+	  
+	  // Add the distance to the DOM
+	  $('ul#mapinfo').append("<li>Du er " + distance/1000 + " km fra huset til Thor Andre med bil</li>");
+	  $('ul#mapinfo').listview("refresh");
+	  });
+		
+		request.fail( function( j, text){
+	  alert("Kunne ikke finne kjøredistanse");
+	});
+}
 
-function CalculateDistance(lat1, lon1, lat2, lon2) {
+// Small helper function
+function toRad(degrees) {
+    return degrees * Math.PI / 180;
+}
+
+function CalculateAirDistance(lat1, lon1, lat2, lon2) {
     //Radius of the earth in:  1.609344 miles,  6371 km  | var R = (6371 / 1.609344);
-    var R = 6371; // Radius of earth in Miles 
+    var R = 6371; // Radius of earth in Km 
     var dLat = toRad(lat2-lat1);
     var dLon = toRad(lon2-lon1); 
+    
+    // Math Stuff
     var a = Math.sin(dLat/2) * Math.sin(dLat/2) +
             Math.cos(toRad(lat1)) * Math.cos(toRad(lat2)) * 
             Math.sin(dLon/2) * Math.sin(dLon/2); 
